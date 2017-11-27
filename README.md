7aec6dfacb132779a79d0326cfdab57653b1c981
JulianMayorga <mayorga.julian@gmail.com>
2017-11-27 14:22:22 -0300

    Trip summary: Show title

---
+++ README.md
@@ -0,0 +1,1 @@
+# Animated modal
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          paddingLeft: 75,
+          paddingTop: 40,
+          paddingRight: 35,
+          paddingBottom: 50,
+          fontFamily: "sans-serif"
+        }}
+      >
+        <h1 style={{ fontSize: "1rem" }}>Trip summary</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
