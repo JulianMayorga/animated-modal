09e8b1d10308aa779bee698f2bf8e1828c9b2e02
JulianMayorga <mayorga.julian@gmail.com>
2017-11-27 19:21:41 -0300

    Trip summary: Show departure details

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 const darkViolet = "#731ED2";
 const lightGrey = "#DCDAE1";
 const black = "#333";
+const yellow = "#E9B308";
 
 const Dot = () => (
   <div
@@ -161,51 +162,251 @@ class App extends Component {
         </div>
         <div
           style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            width: 10,
-            marginBottom: 10
+            display: "grid",
+            gridTemplateAreas: `"a b c d" "a e f d" "a g g g"`,
+            gridTemplateColumns: "10% 30% 30% 30%",
+            gridTemplateRows: "30px 30px",
+            marginBottom: 5
           }}
         >
           <div
             style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
               width: 10,
-              height: 10,
-              backgroundColor: black,
-              marginBottom: 4
+              marginBottom: 10,
+              marginRight: 10,
+              gridArea: "a"
             }}
-          />
-          <Dot />
-          <Dot />
-          <Dot />
-          <Dot />
-          <Dot />
-          <Dot />
-          <Dot />
-          <Dot />
-          <Dot />
-          <Dot />
-          <Dot />
-          <Dot />
-          <Dot />
-          <Dot />
+          >
+            <div
+              style={{
+                width: 10,
+                height: 10,
+                backgroundColor: black,
+                marginBottom: 4
+              }}
+            />
+            <Dot />
+            <Dot />
+            <Dot />
+            <Dot />
+            <Dot />
+            <Dot />
+            <Dot />
+            <Dot />
+            <Dot />
+            <Dot />
+            <Dot />
+            <Dot />
+            <Dot />
+            <Dot />
+          </div>
+          <span
+            style={{
+              fontSize: ".75rem",
+              whiteSpace: "nowrap",
+              gridArea: "b",
+              flex: 1
+            }}
+          >
+            6:25 am
+          </span>
+          <span
+            style={{
+              fontSize: ".65rem",
+              whiteSpace: "nowrap",
+              gridArea: "c",
+              color: lightGrey,
+              flex: 1
+            }}
+          >
+            Paris CDG
+          </span>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-start",
+              gridArea: "d",
+              flexDirection: "column"
+            }}
+          >
+            <div
+              style={{
+                width: 25,
+                height: 25,
+                borderRadius: 25,
+                backgroundColor: yellow,
+                border: `solid 1px ${black}`,
+                margin: 2
+              }}
+            />
+            <span
+              style={{
+                fontSize: ".55rem",
+                color: lightGrey
+              }}
+            >
+              1 h 25 m
+            </span>
+          </div>
+          <span
+            style={{
+              fontSize: ".75rem",
+              whiteSpace: "nowrap",
+              flex: 1,
+              gridArea: "e"
+            }}
+          >
+            7:50 am
+          </span>
+          <span
+            style={{
+              fontSize: ".65rem",
+              whiteSpace: "nowrap",
+              color: lightGrey,
+              flex: 1,
+              gridArea: "f"
+            }}
+          >
+            Munich MUN
+          </span>
+          <div
+            style={{
+              display: "flex",
+              gridArea: "g",
+              fontSize: ".75rem",
+              alignItems: "center",
+              justifyContent: "center",
+              flex: 1
+            }}
+          >
+            <div
+              style={{
+                height: 1,
+                backgroundColor: lightGrey,
+                flex: 1
+              }}
+            />
+            <span
+              style={{
+                backgroundColor: lightGrey,
+                fontSize: ".55rem",
+                padding: 2,
+                borderRadius: 2
+              }}
+            >
+              4 h 20 m
+            </span>
+            <div
+              style={{
+                height: 1,
+                backgroundColor: lightGrey,
+                flex: 1
+              }}
+            />
+          </div>
         </div>
         <div
           style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            width: 10
+            display: "grid",
+            gridTemplateAreas: `"a b c d" "a e f d"`,
+            gridTemplateColumns: "10% 30% 30% 30%",
+            gridTemplateRows: "30px 30px"
           }}
         >
           <div
             style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
               width: 10,
-              height: 10,
-              backgroundColor: black
+              marginBottom: 10,
+              marginRight: 10,
+              gridArea: "a"
             }}
-          />
+          >
+            <div
+              style={{
+                width: 10,
+                height: 10,
+                backgroundColor: black,
+                marginBottom: 4
+              }}
+            />
+          </div>
+          <span
+            style={{
+              fontSize: ".75rem",
+              whiteSpace: "nowrap",
+              gridArea: "b",
+              flex: 1
+            }}
+          >
+            12:10 pm
+          </span>
+          <span
+            style={{
+              fontSize: ".65rem",
+              whiteSpace: "nowrap",
+              gridArea: "c",
+              color: lightGrey,
+              flex: 1
+            }}
+          >
+            Munich MUN
+          </span>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-start",
+              gridArea: "d",
+              flexDirection: "column"
+            }}
+          >
+            <div
+              style={{
+                width: 25,
+                height: 25,
+                borderRadius: 25,
+                backgroundColor: yellow,
+                border: `solid 1px ${black}`,
+                margin: 2
+              }}
+            />
+            <span
+              style={{
+                fontSize: ".55rem",
+                color: lightGrey
+              }}
+            >
+              1 h 25 m
+            </span>
+          </div>
+          <span
+            style={{
+              fontSize: ".75rem",
+              whiteSpace: "nowrap",
+              flex: 1,
+              gridArea: "e"
+            }}
+          >
+            9:15 pm
+          </span>
+          <span
+            style={{
+              fontSize: ".65rem",
+              whiteSpace: "nowrap",
+              color: lightGrey,
+              flex: 1,
+              gridArea: "f"
+            }}
+          >
+            New York NYC
+          </span>
         </div>
       </div>
     );
