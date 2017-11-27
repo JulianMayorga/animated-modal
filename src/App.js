3587d773f60ffb10d29c70e660c15f5e691bf378
JulianMayorga <mayorga.julian@gmail.com>
2017-11-27 14:31:12 -0300

    Trip summary: Show trip price

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+const darkViolet = "#731ED2";
+const lightGrey = "#DCDAE1";
+
 class App extends Component {
   render() {
     return (
@@ -12,7 +15,54 @@ class App extends Component {
           fontFamily: "sans-serif"
         }}
       >
-        <h1 style={{ fontSize: "1rem" }}>Trip summary</h1>
+        <h1
+          style={{
+            fontSize: "1rem",
+            marginBottom: 20
+          }}
+        >
+          Trip summary
+        </h1>
+        <div
+          style={{
+            display: "flex",
+            height: 80,
+            boxShadow: `5px 5px 10px ${lightGrey}`
+          }}
+        >
+          <div style={{ backgroundColor: darkViolet, flexBasis: 80 }} />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              flexGrow: 1,
+              padding: 10,
+              paddingLeft: 20
+            }}
+          >
+            <div
+              style={{
+                fontSize: ".75rem",
+                color: lightGrey,
+                display: "flex",
+                alignItems: "center",
+                flexGrow: 1
+              }}
+            >
+              PAR-NYC
+            </div>
+            <div
+              style={{
+                fontWeight: "bold",
+                display: "flex",
+                alignItems: "center",
+                flexGrow: 1
+              }}
+            >
+              £ 4,000
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
